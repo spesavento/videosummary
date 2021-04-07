6e7a5f742ae522f6386b0de4830f5dd0f51210d0
Sara Pesavento <sarapesavento@gmail.com>
2021-04-07 10:47:59 -0700

    clean up comments
    delete unused files

--- boundingbox.py
+++ boundingbox.py
@@ -1,6 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-# drawing bounding box around objects
+# 1. finds images that have a certain object,
+# 2. draws bounding boxes around the objects and
+# 3. stores the image frames with bounding boxes into a new folder
 
 import cv2
 import sys
@@ -10,35 +12,39 @@ import pymysql # pip3 install PyMySQL
 import os
 from pathlib import Path
 
-# establish a connection to AWS MySQL database
+# for now, manually input the object and video variables here
+# Specify object to find, and video to use
+objecttofind = 'Bicycle'
+# 1 is ucdavis.mp4, this is the video_id in the mysql table
+videotouse = 1
+imageWidth=960
+imageHeight=540
+
+# establish a connection to AWS RDS MySQL database
+# need to import the database password (on Google Doc)
 conn = pymysql.connect(
     database="videometadata",
     host="videometadata.cqcc6zskeglo.us-west-1.rds.amazonaws.com",
     user="admin",
-    password="Twister123!",
+    password="<password here>",
     port=3306,
     )
 cur = conn.cursor()
 
-objecttofind = 'Bicycle'
-videotouse = 1
-
+# get images that have the object objecttofind
+# select rows that match the object detected
 sql = "SELECT * FROM boundingbox WHERE object_detected = %s and video_id = %s ORDER BY frame_number ASC"
 val = (objecttofind, videotouse)
-# cursor.execute(sql, ('webmaster@python.org',))
 cur.execute(sql, val)
 result = cur.fetchall()
 conn.commit()
-
+# store the object bounding box information that was in the mysql table
 for row in result:
     frame_number = row[2]
     bb_width = row[5]
     bb_height = row[6]
     bb_left = row[7]
     bb_top = row[8]
-
-    imageWidth=960
-    imageHeight=540
 
     # calculate the (x,y) coordinates of the bounding box
     # (0,0) is considered the top left corner of the image
@@ -47,12 +53,14 @@ for row in result:
     lowerright_x = int(upperleft_x + (bb_width * imageWidth))
     lowerright_y = int(upperleft_y + (bb_height * imageHeight))
 
-    inImage='/Users/gerrypesavento/Documents/sara/videosummary/localfiles/ucdavis/frames/'+str(frame_number)+'.jpg'
-    outImage='/Users/gerrypesavento/Documents/sara/videosummary/localfiles/ucdavis/summary/bicycle/frames/'+str(frame_number)+'.jpg'
+    # using local file
+    inImage='<your full local path here>/localfiles/ucdavis/frames/'+str(frame_number)+'.jpg'
+    outImage='<your full local path here>/localfiles/ucdavis/summary/bicycle/frames/'+str(frame_number)+'.jpg'
 
+    # if a summary image already has a bounding box, add a new one to the existing frame image
     my_file = Path(outImage)
     if my_file.is_file():
-        inImage='/Users/gerrypesavento/Documents/sara/videosummary/localfiles/ucdavis/summary/bicycle/frames/'+str(frame_number)+'.jpg'
+        inImage='<your full local path here>/localfiles/ucdavis/summary/bicycle/frames/'+str(frame_number)+'.jpg'
         print ('found existing'+str(frame_number))
 
     imgcv = cv2.imread(inImage) # opencv numby array format
--- create_tables.sql
+++ create_tables.sql
@@ -1,28 +1,37 @@
 
+/* this is a dump of SQL commands used on MySQL Workbench */
 
-/* video table
+/* create "video" table
 - short list of the full length videos being analyzed */
-CREATE TABLE IF NOT EXISTS video (
+CREATE TABLE IF NOT EXISTS videometadata.video (
   video_id INT AUTO_INCREMENT PRIMARY KEY,
   videoname VARCHAR(30),
   numframes INT UNSIGNED,
   widthpixels INT UNSIGNED,
   heightpixels INT UNSIGNED,
-  fps INT UNSIGNED,
-  PRIMARY KEY (video_id)
+  fps INT UNSIGNED
 ) ENGINE = InnoDB;
 
-/* boundingbox table
-- storing objects detected (people, objects, text) and bounding box */
-CREATE TABLE IF NOT EXISTS boundingbox (
+/* create "boundingbox" table
+- storing objects detected (people, objects, text) and bounding box
+- this information is derived from AWS rekognition
+*/
+CREATE TABLE IF NOT EXISTS videometadata.boundingbox (
     boundingbox_id INT AUTO_INCREMENT PRIMARY KEY,
     video_id INT UNSIGNED,
     frame_number INT UNSIGNED,
     object_detected VARCHAR(30),
-    confidence VARCHAR(30),
-    bb_width INT UNSIGNED,
-    bb_height INT UNSIGNED,
-    bb_left INT UNSIGNED,
-    bb_top INT UNSIGNED,
-    PRIMARY KEY (boundingbox_id) )
-ENGINE = InnoDB;
+    confidence FLOAT,
+    bb_width FLOAT,
+    bb_height FLOAT,
+    bb_left FLOAT,
+    bb_top FLOAT
+) ENGINE = InnoDB;
+
+/* misc SQL that works using SQL Workbench */
+USE videometadata;
+SELECT * FROM boundingbox;
+ALTER TABLE boundingbox MODIFY COLUMN confidence FLOAT;
+DELETE from boundingbox WHERE boundingbox_id BETWEEN 42 AND 59;
+SELECT * FROM boundingbox WHERE object_detected = 'Bicycle' and video_id = 1 ORDER BY frame_number ASC;
+
